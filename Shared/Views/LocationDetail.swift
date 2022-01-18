00489581014c0001eb8075a9aa1e24fdd11212e6
lcs-vcloutier <vcloutier@lcs.on.ca>
2022-01-18 09:37:01 -0500

    layed out the detail view for location

---
+++ Shared/Views/LocationDetail.swift
@@ -0,0 +1,55 @@
+//
+//  LocationDetail.swift
+//  TrekrMultiPlatform
+//
+//  Created by Vincent Cloutier on 2022-01-18.
+//
+
+import SwiftUI
+
+struct LocationDetail: View {
+    // The location to show details about
+    // A copy of an instance of a struct will be provided to this view
+    let location: Location
+    var body: some View {
+        ScrollView {
+            Image(location.heroPicture)
+                .resizable()
+                .scaledToFit()
+            HStack {
+                Text(location.country)
+                    .font(.title)
+                    .foregroundColor(.secondary)
+                    .multilineTextAlignment(.leading)
+                    .padding(.bottom)
+
+                Spacer()
+            }
+            .padding(.horizontal)
+            
+            Text(location.description)
+                .padding(.horizontal)
+            
+            HStack {
+                Text("Did you know?")
+                    .font(.title3)
+                    .bold()
+                    .padding(.vertical)
+
+                Spacer()
+            }
+            .padding(.horizontal)
+            Text(location.more)
+                .padding([.horizontal, .bottom])
+        }
+        .navigationTitle(location.name)
+    }
+}
+
+struct LocationDetail_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationView {
+            LocationDetail(location: Location.example)
+        }
+    }
+}
