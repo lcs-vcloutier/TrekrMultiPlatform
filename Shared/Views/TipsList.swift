4e8400c9a970804982c9e468c8d60c686c26da20
lcs-vcloutier <vcloutier@lcs.on.ca>
2022-01-18 14:13:46 -0500

    added children key path so list view so children property text

--- Shared/Views/TipsList.swift
+++ Shared/Views/TipsList.swift
@@ -14,7 +14,7 @@ struct TipsList: View {
     
     var body: some View {
 
-        List(tips, id: \.text) { tip in
+        List(tips, id: \.text, children: \.children) { tip in
             
             //if there are children on this particular tip
             if tip.children != nil {
