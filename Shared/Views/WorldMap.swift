63c4e08ee4b1cbab698229262332e17604efdd63
lcs-vcloutier <vcloutier@lcs.on.ca>
2022-01-18 10:03:19 -0500

    got map view centered on eastern NA

--- Shared/Views/WorldMap.swift
+++ Shared/Views/WorldMap.swift
@@ -5,16 +5,30 @@
 //  Created by Vincent Cloutier on 2022-01-18.
 //
 
+import MapKit
 import SwiftUI
 
 struct WorldMap: View {
+    
+    // Centre map on LCS but keep the view wide enough for most of eastern NA
+    @State var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 44.43922, longitude: -78.26571), span: MKCoordinateSpan(latitudeDelta: 40, longitudeDelta: 40))
+    
     var body: some View {
-        Text("WorldMap")
+        
+        
+        
+        Map(coordinateRegion: $region)
+            .navigationTitle("Map")
+        
+        
     }
 }
 
 struct WorldMap_Previews: PreviewProvider {
     static var previews: some View {
-        WorldMap()
+        NavigationView {
+            WorldMap()
+            
+        }
     }
 }
